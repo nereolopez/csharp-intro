96f3ece2009c86fd9d3af00965ed2e8f51dc7e07
nereo.lopez <nereo.lopez@outlook.com>
2018-03-30 11:58:35 +0200

    Unit Testing TDD prepared

---
+++ CarRentalAgency.Tests/Logic/RentalsManagerTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarRentalAgency.Model;
+using CarRentalAgency.Logic;
+using Moq;
+using CarRentalAgency.Services;
+
+namespace CarRentalAgency.Tests.Logic
+{
+    [TestClass]
+    public class RentalsManagerTests
+    {
+        Rental ongoingRental;
+        Car car;
+        const decimal depositFee = 500;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.car = new Car("Mercedes", "C Klasse", EnergyType.Gas, 0, 200, 400, 0.06m, depositFee);
+            this.ongoingRental = new Rental(this.car, "customerId", DateTime.Now, DateTime.Now.AddDays(2), 12345);
+        }
+
+        [TestMethod]
+        public void DepositUnblockedIfCarHasNoDamageTest()
+        {
+            // Arrange
+            bool carIsDamaged = false;
+            var mockCustomersManager = new Mock<ICustomersManager>();
+            var mockCarsManager = new Mock<ICarsManager>();
+            var mockRentalService = new Mock<IRentalService>();
+            var testee = new RentalsManager(mockCustomersManager.Object, mockCarsManager.Object, mockRentalService.Object);
+
+            // Act
+            var closedRental = testee.CloseRental(this.ongoingRental, carIsDamaged);
+
+            // Assert
+            Assert.Equals(closedRental.DepositFee, 0);
+        }
+
+        [TestMethod]
+        public void DepositKeptIfCarHasDamageTest()
+        {
+            // Arrange
+            bool carIsDamaged = false;
+            var mockCustomersManager = new Mock<ICustomersManager>();
+            var mockCarsManager = new Mock<ICarsManager>();
+            var mockRentalService = new Mock<IRentalService>();
+            var testee = new RentalsManager(mockCustomersManager.Object, mockCarsManager.Object, mockRentalService.Object);
+
+            // Act
+            var closedRental = testee.CloseRental(this.ongoingRental, carIsDamaged);
+
+            // Assert
+            Assert.Equals(closedRental.DepositFee, depositFee);
+        }
+
+        [TestMethod]
+        public void ChargeExtraKmsTest()
+        {
+        }
+
+        [TestMethod]
+        public void RentalStatusSetToCloseTest()
+        {
+        }
+
+        // Things we could test in integration
+        // Car is unblocked
+    }
+}
